f006be6aaa5d103606b0e202ac97f1117795e4ff
tatsuya828828 <kkjy011894@gmail.com>
2020-06-19 23:13:40 +0900

    [finish]10-2

--- src/job/SuperHero.java
+++ src/job/SuperHero.java
@@ -19,13 +19,9 @@ public class SuperHero extends Hero{
 	}
 
 	public void attack(Matango m) {
-		System.out.println(this.name+"の攻撃！");
-		m.hp-=5;
-		System.out.println("5ポイントのダメージを与えた！");
+		super.attack(m);
 		if (this.flying) {
-			System.out.println(this.name+"の攻撃！");
-			m.hp -= 5;
-			System.out.println("5ポイントのダメージを与えた！");
+			super.attack(m);
 		}
 	}
 }
